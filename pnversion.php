a93529bb90b7522fae7faa1f65bf8ea5f3880eef
mateo <mateo@431cdd4e-d3a3-42c0-bcbe-994f1ab25577>
2009-12-07 23:31:10 +0000

    cleanup: version file formatting

--- pnversion.php
+++ pnversion.php
@@ -3,18 +3,20 @@
 
 $dom = ZLanguage::getModuleDomain('mediashare');
 
-$modversion['name'] = 'mediashare';
-$modversion['version'] = '4.0.1';
-$modversion['displayname'] = __('Mediashare', $dom);
+$modversion['name']           = 'mediashare';
+$modversion['version']        = '4.0.1';
+$modversion['displayname']    = __('Mediashare', $dom);
+$modversion['description']    = __('Media sharing and gallery module', $dom);
 //! module url should be lowercase without spaces and different to displayname
-$modversion['url'] = __('mediashare', $dom);
-$modversion['description'] = __('Media sharing and gallery', $dom);
-$modversion['credits'] = 'docs/credits.txt';
-$modversion['changelog'] = 'docs/changelog.txt';
-$modversion['license'] = 'docs/copying.txt';
-$modversion['official'] = 0;
-$modversion['author'] = 'Jorn Wildt';
-$modversion['contact'] = 'jw@fjeldgruppen.dk';
-$modversion['admin'] = 1;
-$modversion['user'] = 1;
+$modversion['url']            = __('mediashare', $dom);
+
+$modversion['credits']        = 'docs/credits.txt';
+$modversion['changelog']      = 'docs/changelog.txt';
+$modversion['license']        = 'docs/copying.txt';
+$modversion['official']       = 0;
+$modversion['author']         = 'Jorn Wildt';
+$modversion['contact']        = 'jw@fjeldgruppen.dk';
+$modversion['admin']          = 1;
+$modversion['user']           = 1;
+
 $modversion['securityschema'] = array('mediashare::' => '::');
