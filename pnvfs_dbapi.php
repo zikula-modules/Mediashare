47e6329d429fca25265042e652444f0100ff45e2
mateo <mateo@431cdd4e-d3a3-42c0-bcbe-994f1ab25577>
2009-12-09 08:36:10 +0000

    rework: vfs_dbapi using DBUtil

--- pnvfs_dbapi.php
+++ pnvfs_dbapi.php
@@ -18,27 +18,23 @@ class mediashare_vfsHandlerDB
 
         $fileReference = "vfsdb/$args[baseFileRef]-$args[fileMode].$args[fileType]";
 
-        list ($dbconn) = pnDBGetConn();
-        $pntable = pnDBGetTables();
-
-        $mediadbTable  = $pntable['mediashare_mediadb'];
-        $mediadbColumn = &$pntable['mediashare_mediadb_column'];
+        $pntable       = &pnDBGetTables();
+        $mediadbColumn = $pntable['mediashare_mediadb_column'];
 
         $data  = file_get_contents($filename);
         $bytes = count($data);
 
-        $sql = "INSERT INTO $mediadbTable
-                       ($mediadbColumn[fileref], $mediadbColumn[mode], $mediadbColumn[type], $mediadbColumn[bytes], $mediadbColumn[data])
-                VALUES
-                       ('" . DataUtil::formatForStore($fileReference) . "',
-                        '" . DataUtil::formatForStore($args['fileMode']) . "',
-                        '" . DataUtil::formatForStore($args['fileType']) . "',
-                        $bytes,
-                        '" . DataUtil::formatForStore($data) . "')";
+        $record = array(
+            'fileref' => $fileReference,
+            'mode'    => $args['fileMode'],
+            'type'    => $args['fileType'],
+            'bytes'   => $bytes,
+            'data'    => $data
+        );
 
-        $result = $dbconn->execute($sql);
+        $result = DBUtil::insertObject($record, 'mediashare_mediadb', 'id');
 
-        if ($dbconn->errorNo() != 0) {
+        if ($result === false) {
             return LogUtil::registerError(__f('Error in %1$s: %2$s.', array('vfsHandlerDB.createFile', 'Could not retrieve insert the file information.'), $dom));
         }
 
@@ -49,20 +45,15 @@ class mediashare_vfsHandlerDB
     {
         $dom = ZLanguage::getModuleDomain('mediashare');
 
-        list ($dbconn) = pnDBGetConn();
-        $pntable = pnDBGetTables();
-
-        $mediadbTable  = $pntable['mediashare_mediadb'];
-        $mediadbColumn = &$pntable['mediashare_mediadb_column'];
+        $pntable       = &pnDBGetTables();
+        $mediadbColumn = $pntable['mediashare_mediadb_column'];
 
         $fileReference = DataUtil::formatForStore($fileReference);
+        $where         = "$mediadbColumn[fileref] = '$fileReference'";
+        
+        $result = DBUtil::deleteWhere('mediashare_mediadb', $where);
 
-        $sql = "DELETE FROM $mediadbTable
-                      WHERE $mediadbColumn[fileref] = '$fileReference'";
-
-        $result = $dbconn->execute($sql);
-
-        if ($dbconn->errorNo() != 0) {
+        if ($result === false) {
             return LogUtil::registerError(__f('Error in %1$s: %2$s.', array('vfsHandlerDB.deleteFile', 'Could not delete the file information.'), $dom));
         }
 
@@ -73,11 +64,10 @@ class mediashare_vfsHandlerDB
     {
         $dom = ZLanguage::getModuleDomain('mediashare');
 
-        list ($dbconn) = pnDBGetConn();
-        $pntable = pnDBGetTables();
+        $pntable = &pnDBGetTables();
 
         $mediadbTable  = $pntable['mediashare_mediadb'];
-        $mediadbColumn = &$pntable['mediashare_mediadb_column'];
+        $mediadbColumn = $pntable['mediashare_mediadb_column'];
 
         $data  = file_get_contents($newFilename);
         $bytes = count($data);
@@ -85,12 +75,12 @@ class mediashare_vfsHandlerDB
 
         $sql = "UPDATE $mediadbTable
                    SET $mediadbColumn[data] = '" . DataUtil::formatForStore($data) . "',
-                       $mediadbColumn[bytes] = $bytes
+                       $mediadbColumn[bytes] = '$bytes'
                  WHERE $mediadbColumn[fileref] = '$orgFileReference'";
 
-        $result = $dbconn->execute($sql);
+        $result = DBUtil::executeSQL($sql);
 
-        if ($dbconn->errorNo() != 0) {
+        if ($result === false) {
             return LogUtil::registerError(__f('Error in %1$s: %2$s.', array('vfsHandlerDB.updateFile', 'Could not update the file information.'), $dom));
         }
 
@@ -123,8 +113,7 @@ function mediashare_vfs_dbapi_getMedia($args)
 
     $fileref = DataUtil::formatForStore($args['fileref']);
 
-    list ($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
+    $pntable = &pnDBGetTables();
 
     $mediaTable    = $pntable['mediashare_media'];
     $mediaColumn   = $pntable['mediashare_media_column'];
@@ -150,19 +139,17 @@ function mediashare_vfs_dbapi_getMedia($args)
                AND media.$mediaColumn[title] IS NOT NULL
              WHERE db.$mediadbColumn[fileref] = '$fileref'";
 
-    $result = $dbconn->execute($sql);
+    $result = DBUtil::executeSQL($sql);
 
-    if ($dbconn->errorNo() != 0) {
+    if ($result === false) {
         return LogUtil::registerError(__f('Error in %1$s: %2$s.', array('vfsHandlerDB.getMedia', 'Could not retrieve the file information.'), $dom));
     }
 
-    if ($result->EOF) {
+    if (!$result) {
         return LogUtil::registerError(__('Unknown media item.', $dom));
     }
 
-    $info = array('data' => $result->fields[0], 'mimeType' => $result->fields[1], 'bytes' => $result->fields[2], 'mediaId' => $result->fields[3], 'albumId' => $result->fields[4], 'title' => $result->fields[5], 'modifiedDate' => $result->fields[6]);
-
-    $result->Close();
+    $info = DBUtil::marshallObjects($result, array('data', 'mimeType', 'bytes', 'mediaId', 'albumId', 'title', 'modifiedDate'));
 
     return $info;
 }
