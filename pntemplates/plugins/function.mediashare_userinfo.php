e61ddeeb613a2f5b761a7409ef114b99aa69caf0
Herr.Vorragend <carsten@zikula.org>
2011-08-10 18:04:24 +0200

    More template updates

--- pntemplates/plugins/function.mediashare_userinfo.php
+++ pntemplates/plugins/function.mediashare_userinfo.php
@@ -18,7 +18,7 @@ function smarty_function_mediashare_userinfo($params, $smarty)
 
     $scale     = 1000000;
     $unitTitle = 'Mb';
-    $str       = sprintf("%.2f %s %.2f %s", $size / $scale, __('Of', $dom), $maxSize / $scale, $unitTitle);
+    $str       = sprintf("%.2f %s %.2f %s", $size / $scale, __('of', $dom), $maxSize / $scale, $unitTitle);
 
     $result = "<div class=\"mediashare-userinfo\"><img src=\"$imageDir/bar_left.gif\" height=\"5\" width=\"$leftSize\" alt=\"\" />" . "<img src=\"$imageDir/bar_right.gif\" height=\"5\" width=\"$rightSize\" alt=\"\" />" . " $leftSize% ($str)</div>";
 
--- pnuser.php
+++ pnuser.php
@@ -1,5 +1,4 @@
 <?php
-// $Id$
 //
 // Mediashare by Jorn Wildt (C)
 //
